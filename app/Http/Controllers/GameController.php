d9350da8d9d64a02a4b31f3b6696f022b71978b2
Ryan7AIT <aitmechedalryan7@gmail.com>
2022-01-27 20:10:40 +0100

    some api fixs

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -14,16 +15,21 @@ class GameController extends Controller
      */
     public function index()
     {
+
+        // $befor = Carbon::now()->subMonths(2)->timestamp;
+        // $after = Carbon::now()->addMonths(2)->timestamp;
+
         $popularGames = Http::withHeaders([
             'Client-ID' => 's6p5hps1ozlkh1ubf9150zlxzt56ed',
             'Authorization' => 'Bearer 515b5f8btghk7u3q3bnmn98ffn45cr'
         ])->withBody(
-            'fields *, platforms.*;
-            where rating != null & ( platforms = (48,49,130,6) ) ;
+            'fields *, cover.* , platforms.*;
+            where rating != null & platforms = {48,49,130,6}
+            ;
             sort rating desc;
             limit 12;'
             ,'text/plain')
-            ->post('https://api.igdb.com/v4/games')
+            ->post('https://api.igdb.com/v4/games/')
             ->json();
 
         dump($popularGames);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,174 +4,32 @@
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
         <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
+
+            @foreach ( $popularGames as $game )
+                <div class="game mt-8">
+                    <div class="relative inline-block">
+                        <a href="#">
+                            <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                80%
+                            </div>
                         </div>
                     </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy VII Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/animalcrossing.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
+                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{ $game['name']}}</a>
+                    <div class="text-gray-400 mt-1">
+                        @foreach ($game['platforms'] as $platform)
+                            {{ $platform['abbreviation']}},
+                        @endforeach
                     </div>
                 </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Animal Crossing: New Horizons</a>
-                <div class="text-gray-400 mt-1">Nintendo Switch</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/doom.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Doom Eternal</a>
-                <div class="text-gray-400 mt-1">Playstation 4, PC</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/alyx.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Half Life: Alyx</a>
-                <div class="text-gray-400 mt-1">PC</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/luigi.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Luigi's Mansion 3</a>
-                <div class="text-gray-400 mt-1">Nintendo Switch</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/resident.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Resident Evil 3</a>
-                <div class="text-gray-400 mt-1">PC, Playstation 4, XBox One X</div>
-            </div>
-           <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/resident.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Resident Evil 3</a>
-                <div class="text-gray-400 mt-1">PC, Playstation 4, XBox One X</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/luigi.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Luigi's Mansion 3</a>
-                <div class="text-gray-400 mt-1">Nintendo Switch</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/alyx.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Half Life: Alyx</a>
-                <div class="text-gray-400 mt-1">PC</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/doom.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Doom Eternal</a>
-                <div class="text-gray-400 mt-1">Playstation 4, PC</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/animalcrossing.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Animal Crossing: New Horizons</a>
-                <div class="text-gray-400 mt-1">Nintendo Switch</div>
-            </div>
-            <div class="game mt-8">
-                <div class="relative inline-block">
-                    <a href="#">
-                        <img src="/ff7.jpg" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
-                        <div class="font-semibold text-xs flex justify-center items-center h-full">
-                            80%
-                        </div>
-                    </div>
-                </div>
-                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy VII Remake</a>
-                <div class="text-gray-400 mt-1">Playstation 4</div>
-            </div>
+            @endforeach
+
+
+
+
+
         </div> <!-- end popular-games -->
         <div class="flex flex-col lg:flex-row my-10">
             <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
